d84c0b0f5956b33fefe0c6be57d8cf9cdf29ad52
Katsuya <haradakatsuya190511@gmail.com>
2024-11-12 22:18:36 -0800

    first commit

---
+++ app/src/main/java/com/gmail/haradakatsuya190511/pasta/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.gmail.haradakatsuya190511.pasta
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.gmail.haradakatsuya190511.pasta.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
