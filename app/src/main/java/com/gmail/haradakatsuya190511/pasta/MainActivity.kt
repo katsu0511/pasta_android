79ada28c1688e3670da3ff823a45e546a32d965a
Katsuya <haradakatsuya190511@gmail.com>
2024-11-15 16:47:36 -0800

    リファクタリング

--- app/src/main/java/com/gmail/haradakatsuya190511/pasta/MainActivity.kt
+++ app/src/main/java/com/gmail/haradakatsuya190511/pasta/MainActivity.kt
@@ -4,24 +4,16 @@ import android.media.SoundPool
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
-import com.gmail.haradakatsuya190511.pasta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val soundPool = SoundPool.Builder()
-            .setMaxStreams(1)
-            .build()
-        val soundId = soundPool.load(this, R.raw.full, 1)
-        var soundButton = findViewById<Button>(R.id.play)
-        soundButton.setOnClickListener{
-            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+        val soundPool = SoundPool.Builder().setMaxStreams(1).build()
+        val full = soundPool.load(this, R.raw.full, 1)
+        val playButton = findViewById<Button>(R.id.play)
+        playButton.setOnClickListener{
+            soundPool.play(full, 1.0f, 1.0f, 0, 0, 1.0f)
         }
     }
 }
