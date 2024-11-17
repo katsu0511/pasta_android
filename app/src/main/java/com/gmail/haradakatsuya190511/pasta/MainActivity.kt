05c9d0d67ad3a8888ff45907bf81629ffd156712
Katsuya <haradakatsuya190511@gmail.com>
2024-11-16 17:50:22 -0800

    音声ファイル再生機能を実装

--- app/src/main/java/com/gmail/haradakatsuya190511/pasta/MainActivity.kt
+++ app/src/main/java/com/gmail/haradakatsuya190511/pasta/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.gmail.haradakatsuya190511.pasta
 
-import android.media.SoundPool
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val musicPlayer: MusicPlayer = MusicPlayer(this@MainActivity)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val soundPool = SoundPool.Builder().setMaxStreams(1).build()
-        val full = soundPool.load(this, R.raw.full, 1)
         val playButton = findViewById<Button>(R.id.play)
         playButton.setOnClickListener{
-            soundPool.play(full, 1.0f, 1.0f, 0, 0, 1.0f)
+            if (playButton.text == "＞") {
+                musicPlayer.playFull()
+                playButton.setText(R.string.pause)
+            } else {
+                musicPlayer.pauseFull()
+                playButton.setText(R.string.play)
+            }
         }
 
-        val pasta = soundPool.load(this, R.raw.pasta, 1)
+        val stopButton = findViewById<Button>(R.id.stop)
+        stopButton.setOnClickListener{
+            musicPlayer.stopFull()
+        }
+
         val pastaButton = findViewById<Button>(R.id.pasta)
         pastaButton.setOnClickListener{
-            soundPool.play(pasta, 1.0f, 1.0f, 0, 0, 1.0f)
+            musicPlayer.playPasta()
         }
     }
 }
---
+++ app/src/main/java/com/gmail/haradakatsuya190511/pasta/MusicPlayer.kt
@@ -0,0 +1,35 @@
+package com.gmail.haradakatsuya190511.pasta
+
+import android.content.Context
+import android.media.MediaPlayer
+
+class MusicPlayer(context: Context) {
+
+    private lateinit var fullPlayer: MediaPlayer
+    private lateinit var pastaPlayer: MediaPlayer
+    private val myContext: Context = context
+
+    fun playFull() {
+        fullPlayer = MediaPlayer.create(myContext, R.raw.full)
+        fullPlayer.isLooping = false
+        fullPlayer.start()
+    }
+
+    fun pauseFull() {
+        fullPlayer.pause()
+    }
+
+    fun stopFull() {
+        fullPlayer.stop()
+        fullPlayer.reset()
+        fullPlayer.release()
+    }
+
+    fun playPasta() {
+        if (!::pastaPlayer.isInitialized || !pastaPlayer.isPlaying) {
+            pastaPlayer = MediaPlayer.create(myContext, R.raw.pasta)
+            pastaPlayer.isLooping = false
+            pastaPlayer.start()
+        }
+    }
+}
