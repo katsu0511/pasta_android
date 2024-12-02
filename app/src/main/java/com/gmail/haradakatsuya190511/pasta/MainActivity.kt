ff0a45d49f2efb9ade8b2b1e63e49305c01bc7df
Katsuya <haradakatsuya190511@gmail.com>
2024-12-02 12:50:51 -0800

    ボタンの処理を修正

--- app/src/main/java/com/gmail/haradakatsuya190511/pasta/MainActivity.kt
+++ app/src/main/java/com/gmail/haradakatsuya190511/pasta/MainActivity.kt
@@ -2,7 +2,9 @@ package com.gmail.haradakatsuya190511.pasta
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,29 +14,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val playButton = findViewById<Button>(R.id.play)
+        val playButton = findViewById<ImageButton>(R.id.play)
         playButton.setOnClickListener{
-            if (playButton.text == "＞") {
+            if (playButton.background.constantState == ContextCompat.getDrawable(this, R.drawable.play)!!.constantState) {
                 musicPlayer.playFull()
-                playButton.setText(R.string.pause)
+                playButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pause));
             } else {
                 musicPlayer.pauseFull()
-                playButton.setText(R.string.play)
+                playButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.play));
             }
         }
 
-        val stopButton = findViewById<Button>(R.id.stop)
+        val stopButton = findViewById<ImageButton>(R.id.stop)
         stopButton.setOnClickListener{
             musicPlayer.stopFull()
-            playButton.setText(R.string.play)
+            playButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.play));
         }
 
-        val forwardButton = findViewById<Button>(R.id.forward)
+        val forwardButton = findViewById<ImageButton>(R.id.forward)
         forwardButton.setOnClickListener{
             musicPlayer.forward()
         }
 
-        val rewindButton = findViewById<Button>(R.id.rewind)
+        val rewindButton = findViewById<ImageButton>(R.id.rewind)
         rewindButton.setOnClickListener{
             musicPlayer.rewind()
         }
